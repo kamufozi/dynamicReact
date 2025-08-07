6f7f6b1fa75e3731370b2c1e1a0d914cd2c3fa6e
Fozi Chris <kamufozi679@gmail.com>
2025-08-07 11:35:48 +0200

    Added the bottom section

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,22 +1,19 @@
-import type React from "react";
 import { useState } from "react";
 
 export default function Main(){
-    const [ingredients,setIngredients] = useState<string[]>(['amata','umushyushyo'])
+    const [ingredients,setIngredients] = useState<string[]>([])
     const ingredientListItems = ingredients.map(i=>(
         <li key={i}>{i}</li>
     ))
-    const handleSubmit = function (event: React.FormEvent<HTMLFormElement>){
-        event.preventDefault();
-        const formData = new FormData(event.currentTarget)
-        const inputx  = formData.get("ingredient")
+    const handleSubmit = function (FormData:FormData){
+        const inputx  = FormData.get("ingredient")
         if (typeof(inputx)==="string"){
         setIngredients(prevIngredient=>[...prevIngredient,inputx])
 
     }}
     return (
         <main className="px-[30px] py-[10px] mt-4">
-            <form className="flex justify-center gap-[12px]" onSubmit={handleSubmit}>
+            <form className="flex justify-center gap-[12px]" action={handleSubmit}>
                 <input
                  className="border-[1px] shadow-md grow-1 min-w-[150px]  max-w-[450px] px-5"
                  type="text"
@@ -24,9 +21,21 @@ export default function Main(){
                  aria-label="Add ingredient" />
                  <button className="border-[1px] bg-[#141413] text-white px-5 py-2 rounded-sm ">+ Add ingredient</button>
             </form>
+            {ingredients.length>0&&<section className="flex flex-col gap-18 mt-5">
+            <div>
+            <h2 className="font-medium text-3xl">Ingredients on hand:</h2>
             <ul className="list-disc pl-5 mt-2.5">
                 {ingredientListItems}
             </ul>
+            </div>
+            <div className="flex justify-between items-center rounded-[8px] bg-[#F0EFEB] p-5 ">
+                <div>
+                    <h3 className="text-[1.125rem] font-[500]">Ready for a recipe?</h3>
+                    <p className="text-[#6B7280]  mt-4 ">Generete a recipe from your list of ingredients</p>
+                </div>
+                <button className="rounded-[6px] bg-[#D17557] text-[#FAFAF8] px-5 py-2  font-sans cursor-pointer">Get a recipe</button>
+            </div>
+            </section>}
         </main>
     )
 }
